66e64753900ce87816925142c8c59ef6c3bc60f5
Saqib Muhammad Saeed <saqib.saeed.101@outlook.com>
2021-06-03 16:21:43 +0500

    Add project files.

---
+++ LearnASPNETCoreMVCWithRealApps/Controllers/ProductController.cs
@@ -0,0 +1,48 @@
+﻿using LearnASPNETCoreMVCWithRealApps.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnASPNETCoreMVCWithRealApps.Controllers
+{
+    [Route("product")]
+    public class ProductController : Controller
+    {
+        [Route("")]
+        [Route("index")]
+        [Route("~/")]
+        public IActionResult Index()
+        {
+            return View("Index", new Product());
+        }
+
+
+        [Route("save")]
+        [HttpPost]
+        public IActionResult Save(Product product, IFormFile[] photos)
+        {
+            if (photos == null || photos.Length == 0)
+            {
+                return Content("File(s) not selected");
+            }
+            else
+            {
+                product.Photos = new List<string>();
+                foreach (IFormFile photo in photos)
+                {
+                    var path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/images", photo.FileName);
+                    var stream = new FileStream(path, FileMode.Create);
+                    photo.CopyToAsync(stream);
+                    product.Photos.Add(photo.FileName);
+                }
+            }
+            ViewBag.product = product;
+            return View("Success");
+        }
+
+    }
+}
---
+++ LearnASPNETCoreMVCWithRealApps/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearnASPNETCoreMVCWithRealApps.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public List<string> Photos { get; set; }
+    }
+}
